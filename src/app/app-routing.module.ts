7e0d76f9c84b9c05fbb9f192df37fec10582b177
Anil <anilinside@gmail.com>
2022-04-15 17:48:41 -0400

    adding MultiRepairBay Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MultiRepairBayComponent } from './multi-repair-bay/multi-repair-bay.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'multirepairBay', component:MultiRepairBayComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/multi-repair-bay/multi-repair-bay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-repair-bay',
+  templateUrl: './multi-repair-bay.component.html',
+  styleUrls: ['./multi-repair-bay.component.css']
+})
+export class MultiRepairBayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
